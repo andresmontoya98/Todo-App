811d3f0d61445c20400ee53e0d062acb76f27879
@Andresmontoya985 <1dandresmontoya@gmail.com>
2023-10-15 18:52:21 +0200

    comentario final

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,6 +1,6 @@
 import { Todo } from "../todos/models/todo-model";
 
-const Filters = {
+export const Filters = {
     All: 'all',
     Completed: 'Completed',
     Pending: 'Pending'
@@ -9,21 +9,102 @@ const Filters = {
 
 const state = {
     todos: [
-        new Todo('Piedra del alma'),
-        new Todo('Piedra del infinito'),
-        new Todo('Piedra del infierno'),
-        new Todo('Piedra del tiempo'),
+        // new Todo('Piedra del alma'),
+        // new Todo('Piedra del infinito'),
+        // new Todo('Piedra del realidad'),
+        // new Todo('Piedra del tiempo'),
+        // new Todo('Piedra del poder'),
     ],
     filter: Filters.All,
 }
 
 
 const initStore = () => {
-    console.log(state);
+    loadStore();
     console.log('InitStore 🥑');
 }
 
+const loadStore = () => {
+    if (!localStorage.getItem('state')) return;
+
+    const { todos = [], filter = Filters.All } = JSON.parse(localStorage.getItem('state'));
+    state.todos = todos;
+    state.filter = filter;
+
+}
+
+const saveStateToLocaleStorage = () => {
+    localStorage.setItem('state', JSON.stringify(state));
+}
+
+const getTodos = (filter = Filters.All) => {
+    switch (filter) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter(todo => todo.done);
+        case Filters.Pending:
+            return state.todos.filter(todo => !todo.done);
+        default:
+            throw new Error(`Option ${filter} is not valid`);
+
+    }
+}
+
+const addTodo = (description) => {
+    if (!description) throw new Error('Description is required');
+    state.todos.push(new Todo(description));
+
+    saveStateToLocaleStorage();
+
+}
+
+const toggleTodo = (todoId) => {
+    state.todos = state.todos.map(todo => {
+        if (todo.id === todoId) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
+
+    saveStateToLocaleStorage();
+
+}
+
+const deleteTodo = (todoId) => {
+    state.todos = state.todos.filter(todo => todo.id !== todoId);
+
+    saveStateToLocaleStorage();
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter(todo => !todo.done);
+
+    saveStateToLocaleStorage();
+
+}
+
+const setFilter = (newFilter = Filters.All) => {
+    state.filter = newFilter;
+    saveStateToLocaleStorage();
+
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
+
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
+
 
 }
